0970d8e3d6348c7bef01a2802d59ba14329bd61e
Serchinastico <54cymru@gmail.com>
2019-01-28 11:55:30 +0100

    Create empty edit super hero activity

---
+++ app/src/main/java/com/karumi/ui/presenter/EditSuperHeroPresenter.kt
@@ -0,0 +1,8 @@
+package com.karumi.ui.presenter
+
+import android.arch.lifecycle.LifecycleObserver
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.MainScope
+
+class EditSuperHeroPresenter : LifecycleObserver, CoroutineScope by MainScope() {
+}
