aa5c0feefdf86b57fa1e2208e0bca203a4d8dab6
Davide Mendolia <davide@gokarumi.com>
2020-03-12 09:04:20 +0100

    update recordings

--- scripts/create_emulator.sh
+++ scripts/create_emulator.sh
@@ -5,17 +5,15 @@ if $ANDROID_HOME/tools/android list avd | grep -q screenshot-test; then
     exit 0;
 fi
 
-echo "Creating a brand new SDCard..."
+echo "Cleanup old SDCard..."
 rm -rf sdcard.img
-$ANDROID_HOME/tools/mksdcard -l e 1G sdcard.img
-echo "SDCard created!"
 
 echo "Downloading the image to create the emulator..."
 echo no | $ANDROID_HOME/tools/bin/sdkmanager "system-images;android-22;default;x86_64"
 echo "Image downloaded!"
 
 echo "Creating the emulator to run screenshot tests..."
-echo no | $ANDROID_HOME/tools/bin/avdmanager --verbose create avd --force --name "screenshot-test" --abi default/x86_64 --package "system-images;android-22;default;x86_64" --sdcard sdcard.img
+echo no | $ANDROID_HOME/tools/bin/avdmanager --verbose create avd --force --name "screenshot-test" --abi default/x86_64 --package "system-images;android-22;default;x86_64" -c 1G
 echo "Emulator created!"
 
 echo "Editing emulator configuration..."
