00d1e1c4ea81646480573348d2ca77977c8202ea
Pedro Vicente Gómez Sánchez <pedro@karumi.com>
2020-03-17 16:53:38 +0100

    Try with an emulator configuration we already know it is working from a different project

--- scripts/create_emulator.sh
+++ scripts/create_emulator.sh
@@ -5,15 +5,17 @@ if $ANDROID_HOME/tools/android list avd | grep -q screenshot-test; then
     exit 0;
 fi
 
-echo "Cleanup old SDCard..."
+echo "Creating a brand new SDCard..."
 rm -rf sdcard.img
+$ANDROID_HOME/tools/mksdcard -l e 1G sdcard.img
+echo "SDCard created!"
 
 echo "Downloading the image to create the emulator..."
-echo no | $ANDROID_HOME/tools/bin/sdkmanager "system-images;android-27;default;x86_64"
+echo no | $ANDROID_HOME/tools/bin/sdkmanager "system-images;android-22;default;x86_64"
 echo "Image downloaded!"
 
 echo "Creating the emulator to run screenshot tests..."
-echo no | $ANDROID_HOME/tools/bin/avdmanager --verbose create avd --force --name "screenshot-test" --abi default/x86_64 --package "system-images;android-27;default;x86_64" -c 1G
+echo no | $ANDROID_HOME/tools/bin/avdmanager --verbose create avd --force --name "screenshot-test" --abi default/x86_64 --package "system-images;android-22;default;x86_64" --sdcard sdcard.img
 echo "Emulator created!"
 
 echo "Editing emulator configuration..."
