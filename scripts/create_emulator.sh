b2a25cdcae2c14073312e9bc1628bbbc0b1a36ef
Davide Mendolia <davide@gokarumi.com>
2020-03-18 09:55:56 +0100

    Update create_emulator.sh

--- scripts/create_emulator.sh
+++ scripts/create_emulator.sh
@@ -9,14 +9,15 @@ echo "Cleanup old SDCard..."
 rm -rf sdcard.img
 
 echo "Downloading the image to create the emulator..."
-echo no | $ANDROID_HOME/tools/bin/sdkmanager "system-images;android-27;default;x86_64"
+echo no | $ANDROID_HOME/tools/bin/sdkmanager "system-images;android-22;default;x86_64"
 echo "Image downloaded!"
 
 echo "Creating the emulator to run screenshot tests..."
-echo no | $ANDROID_HOME/tools/bin/avdmanager --verbose create avd --force --name "screenshot-test" --abi default/x86_64 --package "system-images;android-27;default;x86_64" -c 1G
+echo no | $ANDROID_HOME/tools/bin/avdmanager --verbose create avd --force --name "screenshot-test" --abi default/x86_64 --package "system-images;android-22;default;x86_64" -C 1G
 echo "Emulator created!"
 
 echo "Editing emulator configuration..."
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cp $DIR/config.ini ~/.android/avd/screenshot-test.avd/config.ini
 echo "Emulator edited!"
+cat ~/.android/avd/screenshot-test.avd/config.ini
